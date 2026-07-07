a5f7bfe895427586cce1b24a643c6a70b2ac7c43
JeromeJason <jasonnem048@gmail.com>
2026-07-07 17:17:07 +0300

    doc: add app content

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "@/components/Navbar";
+import { ProtectedRoute } from "@/components/ProtectedRoute";
+import Home from "@/pages/Home";
+import Products from "@/pages/Products";
+import ProductDetails from "@/pages/ProductDetails";
+import Cart from "@/pages/Cart";
+import Checkout from "@/pages/Checkout";
+import Login from "@/pages/Login"; 
+import { Register } from "@/pages/Register";
+import { Contact }  from "@/pages/Contact";
+
+export default function App() {
+  return (
+    <div className="flex min-h-screen flex-col bg-paper">
+      <Navbar />
+
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />}/>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/checkout"
+            element={
+              <ProtectedRoute>
+                <Checkout />
+              </ProtectedRoute>
+            }
+          />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </main>
+     
+    </div>
+  );
+}
